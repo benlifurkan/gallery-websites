d7c02311446016ade891346b22147a1e2f979f92
Abdulbaki Dursun <eggsydev@gmail.com>
2022-07-08 18:29:23 +0300

    feat: add ts path aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from "vite";
 import Vue from "@vitejs/plugin-vue";
 import Icons from "unplugin-icons/vite";
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "./src"),
+    },
+  },
   plugins: [
     Vue(),
     Icons({
