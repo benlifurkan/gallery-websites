3b58e886c5b5dcbd440f43739669dd4b4e4bd256
Abdulbaki Dursun <eggsydev@gmail.com>
2022-07-08 16:12:58 +0300

    feat: create modal component

---
+++ src/components/Modal.vue
@@ -0,0 +1,88 @@
+<script lang="ts" setup>
+import { computed } from "vue";
+import { useImageStore } from "../stores/images";
+import { useImageModalStore } from "../stores/modal";
+import { useBackgroundStore } from "../stores/background";
+
+import ChevronLeft from "~icons/mdi/chevron-left";
+import ChevronRight from "~icons/mdi/chevron-right";
+
+const modal = useImageModalStore();
+const imageStore = useImageStore();
+const backgroundStore = useBackgroundStore();
+
+const getProxifiedImage = computed(
+  () => `https://proxy.duckduckgo.com/iu/?u=${encodeURIComponent(modal.image)}`
+);
+
+const handlePagination = (
+  event: MouseEvent,
+  direction: "previous" | "next"
+) => {
+  event.stopPropagation();
+
+  const imageStoreImages = imageStore.images;
+  const currentIndex = imageStoreImages.findIndex(
+    (item) => item === modal.image
+  );
+
+  if (currentIndex === -1) return;
+
+  const nextItem =
+    direction === "previous"
+      ? imageStoreImages[currentIndex - 1]
+      : imageStoreImages[currentIndex + 1];
+
+  if (!nextItem) return;
+
+  modal.setImage(nextItem);
+};
+</script>
+
+<template>
+  <Teleport to="body">
+    <transition name="fade" mode="out-in">
+      <div
+        v-show="modal.isVisible"
+        class="fixed inset-0 z-30 flex items-center justify-center bg-black/75"
+        @click="() => modal.toggle()"
+      >
+        <div @click="(e) => e.stopPropagation()">
+          <div
+            class="bg-center bg-cover rounded-xl"
+            :style="{
+              backgroundImage: `url('${getProxifiedImage}')`,
+            }"
+          >
+            <img
+              :src="getProxifiedImage"
+              alt="gallery image"
+              class="invisible max-h-[90vh] max-w-[90vw]"
+            />
+          </div>
+        </div>
+
+        <!-- Controls -->
+        <div
+          class="fixed inset-x-0 bottom-0 flex items-center justify-center w-full text-white lg:bottom-auto lg:justify-between"
+        >
+          <button
+            class="p-1 m-6 transition-opacity rounded-full shadow-md hover:opacity-50"
+            :class="backgroundStore.currentBackground"
+            @click="(e) => handlePagination(e, 'previous')"
+          >
+            <ChevronLeft class="w-8 h-8" />
+          </button>
+
+          <button
+            class="p-1 m-6 transition-opacity rounded-full shadow-md hover:opacity-50"
+            :class="backgroundStore.currentBackground"
+            @click="(e) => handlePagination(e, 'next')"
+          >
+            <ChevronRight class="w-8 h-8" />
+          </button>
+        </div>
+      </div>
+    </transition>
+  </Teleport>
+</template>
