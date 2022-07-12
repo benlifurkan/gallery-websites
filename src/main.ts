f522e8a9f556d9abf984c17fbd5ddb6596f4bb43
Abdulbaki Dursun <eggsydev@gmail.com>
2022-07-12 21:22:48 +0300

    chore: remove unused transitions file

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,7 @@ import { createPinia } from "pinia";
 import router from "./router";
 
 import App from "./App.vue";
-
 import "./assets/tailwind.css";
-import "./styles/transitions.scss";
 
 const pinia = createPinia();
 const app = createApp(App);
