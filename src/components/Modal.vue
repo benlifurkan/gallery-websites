cc402f4087683e50f8b5747404d828a9f9a6fdda
Abdulbaki Dursun <eggsydev@gmail.com>
2022-07-12 21:18:10 +0300

    chore: add scoped fade transition

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -114,3 +114,15 @@ onBeforeMount(() => {
     </Transition>
   </Teleport>
 </template>
+
+<style scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.2s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
